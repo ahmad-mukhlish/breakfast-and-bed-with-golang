4e6052c15fefd54c4826d708b063153ca0f32d3d
Ahmad Mukhlis Saputra <ahmad.mukhlish.saputra@gmail.com>
2023-12-14 21:29:27 +0700

    add required test

--- internal/form/form_validator_test.go
+++ internal/form/form_validator_test.go
@@ -2,12 +2,13 @@ package form
 
 import (
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
 func TestFormValidator_IsValid(t *testing.T) {
 	mockedReq := httptest.NewRequest("POST", "/testo", nil)
-	form := mockedReq.Form
+	form := mockedReq.PostForm
 
 	validator := NewValidator(form)
 
@@ -15,6 +16,40 @@ func TestFormValidator_IsValid(t *testing.T) {
 
 	if !isValid {
 		t.Errorf("Should not return error")
+	}
+
+}
+
+func TestFormValidator_Required(t *testing.T) {
+
+	mockedReq := httptest.NewRequest("POST", "/testo", nil)
+
+	postForm := url.Values{}
+
+	postForm.Add("a", "a")
+	postForm.Add("b", "a")
+	postForm.Add("c", "a")
+
+	mockedReq.PostForm = postForm
+
+	validatorErrorCase := NewValidator(mockedReq.PostForm)
+
+	validatorErrorCase.Required("a", "zzz")
+
+	errorCase := validatorErrorCase.IsValid()
+
+	if errorCase {
+		t.Errorf("Should not passed, error expected")
+	}
+
+	validatorPassedCase := NewValidator(mockedReq.PostForm)
+
+	validatorPassedCase.Required("a", "b", "c")
+
+	passedCase := validatorPassedCase.IsValid()
+
+	if !passedCase {
+		t.Errorf("Should be passed")
 	}
 
 }
