c11a5c0e1a3a6c50f4f80ac80a734a3b57615e7f
Ahmad Mukhlis Saputra <ahmad.mukhlish.saputra@gmail.com>
2023-12-15 19:58:04 +0700

    finish validate package form

--- internal/form/form_validator_test.go
+++ internal/form/form_validator_test.go
@@ -39,7 +39,7 @@ func TestFormValidator_Required(t *testing.T) {
 	errorMessage := validatorErrorCase.FormError.GetFirstErrorMessage("zzz")
 
 	if errorMessage == "" {
-		t.Errorf("Expected error message is filled, because zzz is required but not filled")
+		t.Errorf("Expected error message is filled")
 	}
 
 	validatorPassedCase := NewValidator(postForm)
@@ -58,4 +58,122 @@ func TestFormValidator_Required(t *testing.T) {
 		t.Errorf("Expected error message is none, because the form should be valid")
 	}
 
+}
+
+func TestFormValidator_Has(t *testing.T) {
+	postForm := url.Values{}
+
+	postForm.Add("a", "a")
+	postForm.Add("b", "a")
+	postForm.Add("c", "a")
+
+	validatorErrorCase := NewValidator(postForm)
+
+	validatorErrorCase.Has("zzz")
+
+	errorCase := validatorErrorCase.IsValid()
+
+	if errorCase {
+		t.Errorf("Should not passed, error expected")
+	}
+
+	errorMessage := validatorErrorCase.FormError.GetFirstErrorMessage("zzz")
+
+	if errorMessage == "" {
+		t.Errorf("Expected error message is filled")
+	}
+
+	validatorPassedCase := NewValidator(postForm)
+
+	validatorPassedCase.Has("a")
+
+	passedCase := validatorPassedCase.IsValid()
+
+	if !passedCase {
+		t.Errorf("Should be passed")
+	}
+
+	errorMessageEmpty := validatorPassedCase.FormError.GetFirstErrorMessage("a")
+
+	if errorMessageEmpty != "" {
+		t.Errorf("Expected error message is none, because the form should be valid")
+	}
+
+}
+
+func TestFormValidator_ValidateLength(t *testing.T) {
+	postForm := url.Values{}
+
+	postForm.Add("a", "abajadun")
+
+	validatorErrorCase := NewValidator(postForm)
+
+	validatorErrorCase.ValidateLength("a", 10)
+
+	errorCase := validatorErrorCase.IsValid()
+
+	if errorCase {
+		t.Errorf("Should not passed, error expected")
+	}
+
+	errorMessage := validatorErrorCase.FormError.GetFirstErrorMessage("a")
+
+	if errorMessage == "" {
+		t.Errorf("Expected error message is filled")
+	}
+
+	validatorPassedCase := NewValidator(postForm)
+
+	validatorPassedCase.ValidateLength("a", 3)
+
+	passedCase := validatorPassedCase.IsValid()
+
+	if !passedCase {
+		t.Errorf("Should be passed")
+	}
+
+	errorMessageEmpty := validatorPassedCase.FormError.GetFirstErrorMessage("a")
+
+	if errorMessageEmpty != "" {
+		t.Errorf("Expected error message is none, because the form should be valid")
+	}
+}
+
+func TestFormValidator_ValidateEmail(t *testing.T) {
+	postForm := url.Values{}
+
+	postForm.Add("a", "abajadun")
+	postForm.Add("b", "abajadun@gmail.com")
+
+	validatorErrorCase := NewValidator(postForm)
+
+	validatorErrorCase.ValidateEmail("a")
+
+	errorCase := validatorErrorCase.IsValid()
+
+	if errorCase {
+		t.Errorf("Should not passed, error expected")
+	}
+
+	errorMessage := validatorErrorCase.FormError.GetFirstErrorMessage("a")
+
+	if errorMessage == "" {
+		t.Errorf("Expected error message is filled")
+	}
+
+	validatorPassedCase := NewValidator(postForm)
+
+	validatorPassedCase.ValidateEmail("b")
+
+	passedCase := validatorPassedCase.IsValid()
+
+	if !passedCase {
+		t.Errorf("Should be passed")
+	}
+
+	errorMessageEmpty := validatorPassedCase.FormError.GetFirstErrorMessage("a")
+
+	if errorMessageEmpty != "" {
+		t.Errorf("Expected error message is none, because the form should be valid")
+	}
 }
